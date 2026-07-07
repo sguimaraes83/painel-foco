ee27c051056cd94eb63dd69efba8e035ea540fba
Samuel Guimarães <samuca.guimaraes@gmail.com>
2026-07-07 16:32:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,5 @@
 // definidas em schema.sql, não por essa chave.
 // ============================================================
 
-const SUPABASE_URL = 'COLE_AQUI_A_SUA_PROJECT_URL';
-const SUPABASE_ANON_KEY = 'COLE_AQUI_A_SUA_ANON_KEY';
+const SUPABASE_URL = 'https://xdepdsrntstbcsgauzco.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZXBkc3JudHN0YmNzZ2F1emNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0NDI3NTIsImV4cCI6MjA5OTAxODc1Mn0.e8MXOlE7tuFDl0CIYoMTJ894SGoC7ikBnly3k40dW-4';
